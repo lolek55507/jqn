c17fe9a370c1f0ff2bc98fb07212b527e9136c6f
lolek55507 <110234766+lolek55507@users.noreply.github.com>
2023-09-17 18:41:57 +0100

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,35 @@
+// All jquery statements start with the dollar sight "$"
+// JQuery often selects an HTML element with a selector, then does something to that element
+// for example to make button bounce 
+// $("button").addClass("animated bounce");
+
+// $("button").addClass("animated bounce");
+  //  $(".well").addClass("animated shake");
+ //   $("#target3").addClass("animated fadeOut");
+
+
+ // $(document).ready(function() {
+ // $("button").addClass("animated");
+ // $(".btn").addClass("shake");
+ // $("#target1").addClass("btn-primary")});
+
+ // $("button").removeClass("btn-default");
+ // $(".well").removeClass("btn-default");
+ // $("#target3").removeClass("btn-default");
+
+ // $("#target1").css("color", "red");
+ // $("#target1").prop("disabled", true);
+ // $("#target4").html("<em>#target4</em>");
+ // $("#target4").remove();
+ // $("#target2").appendTo("#right-well");
+ // function chaining is calling methods one after another or calling function in same line and used on the same element for example elem.split("").reverse().joind("");
+ // $("#target5").clone().appendTo("#left-well");
+ // $("#target1").parent().css("background-color", "red");
+ // $("#right-well").children().css("color", "orange");
+ // $(".target:nth-child(2)").addClass("animated bounce");
+// JQuery is zero-indexed which mean the first element in a selection has a position of 0.
+// This can be a little confusing as, counter-intuitively, :odd selects the 2nd element (position 1), fourth element (position 3), and so one.
+// Here is how you would target all the odd elements with class target and give them classes
+// $(".target:odd").addClass("animated shake"); // for even
+// $(".target:even").addClass("animated shake"); for odd
+// $("body").addClass("animated hinge body");
